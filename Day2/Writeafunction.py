37312f7e48a5ad277efae6c9b122101e949e8cd9
BweepBwop <a.kelsey.magat@gmail.com>
2025-01-02 06:47:52 +0800

    Update Writeafunction.py

--- Day2/Writeafunction.py
+++ Day2/Writeafunction.py
@@ -4,9 +4,15 @@ def is_leap(year):
     if year % 100:
         return False
     elif year % 400:
-        return True 
+        return True
+    elif year % 4:
+        return True
     else:
         False
 
+    if 1900 <= year <= 10**5:
+        print(is_leap(year))
+    else:
+        print("Year out of range")
 year = int(input())
 print(is_leap(year))
